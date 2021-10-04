bf8b3fe3f5b0e493a1655342e53ff3e35390ce8b
Nicholas Monath <nmonath@cs.umass.edu>
2021-10-03 20:42:15 -0400

    numpy version fix

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ llamac_module = Extension('llamac',
 
 setuptools.setup(
     name="graphgrove",
-    version="0.0.10",
+    version="0.0.11",
     author="Nicholas Monath",
     author_email="nmonath@cs.umass.edu",
     description="Building flat, tree, and DAG structured clusterings.",
